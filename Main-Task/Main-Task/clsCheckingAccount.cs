037ebfde990a0db308cec430806df5608021bb9e
Ahmed Esam <buhussien2@gmail.com>
2025-07-23 12:22:13 +0300

    Add files via upload

---
+++ Main-Task/Main-Task/clsCheckingAccount.cs
@@ -0,0 +1,16 @@
+namespace Main_Task;
+
+public class CheckingAccount : Account
+{
+    private const double Fee = 1.5;
+
+    public CheckingAccount(string name,double balance = 0) : base(name, balance)
+    {
+    }
+
+    public override bool Withdraw(double amount)
+    {
+        return base.Withdraw(amount + Fee);
+    }
+    
+}

a8dd3463dd1e20640303ea9ac97bb8002a1edb94
Ahmed Esam <buhussien2@gmail.com>
2025-08-13 10:12:16 +0300

    Update Users.cs

--- Task-5/Users.cs
+++ Task-5/Users.cs
@@ -1,6 +1,7 @@
 namespace Task_5;
-public abstract class User { }
-
+public abstract class User {
+    // Maybe add the username and password later, So I did an inheritance.
+}
 public class Teacher : User
 {
     public void AddQuestion(Question question, Quiz quiz)
